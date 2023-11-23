80772716c3201694a738e7b98b72d44db5863dbd
GadzZzilo <GadzZzilo@yandex.ru>
2023-11-23 14:04:01 +0300

    ''

---
+++ icons.py
@@ -0,0 +1,240 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.5.2
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x026\
+<\
+!DOCTYPE svg PUB\
+LIC \x22-//W3C//DTD\
+ SVG 1.1//EN\x22 \x22h\
+ttp://www.w3.org\
+/Graphics/SVG/1.\
+1/DTD/svg11.dtd\x22\
+>\x0a\x0d<!-- Uploaded\
+ to: SVG Repo, w\
+ww.svgrepo.com, \
+Transformed by: \
+SVG Repo Mixer T\
+ools -->\x0a<svg wi\
+dth=\x22800px\x22 heig\
+ht=\x22800px\x22 viewB\
+ox=\x220 0 24 24\x22 f\
+ill=\x22none\x22 xmlns\
+=\x22http://www.w3.\
+org/2000/svg\x22>\x0a\x0d\
+<g id=\x22SVGRepo_b\
+gCarrier\x22 stroke\
+-width=\x220\x22/>\x0a\x0d<g\
+ id=\x22SVGRepo_tra\
+cerCarrier\x22 stro\
+ke-linecap=\x22roun\
+d\x22 stroke-linejo\
+in=\x22round\x22/>\x0a\x0d<g\
+ id=\x22SVGRepo_ico\
+nCarrier\x22> <path\
+ d=\x22M4 12H20M12 \
+4V20\x22 stroke=\x22#f\
+fffff\x22 stroke-wi\
+dth=\x222\x22 stroke-l\
+inecap=\x22round\x22 s\
+troke-linejoin=\x22\
+round\x22/> </g>\x0a\x0d<\
+/svg>\
+\x00\x00\x04\xc2\
+<\
+!DOCTYPE svg PUB\
+LIC \x22-//W3C//DTD\
+ SVG 1.1//EN\x22 \x22h\
+ttp://www.w3.org\
+/Graphics/SVG/1.\
+1/DTD/svg11.dtd\x22\
+>\x0a\x0d<!-- Uploaded\
+ to: SVG Repo, w\
+ww.svgrepo.com, \
+Transformed by: \
+SVG Repo Mixer T\
+ools -->\x0a<svg wi\
+dth=\x22800px\x22 heig\
+ht=\x22800px\x22 viewB\
+ox=\x220 0 24 24\x22 f\
+ill=\x22none\x22 xmlns\
+=\x22http://www.w3.\
+org/2000/svg\x22>\x0a\x0d\
+<g id=\x22SVGRepo_b\
+gCarrier\x22 stroke\
+-width=\x220\x22/>\x0a\x0d<g\
+ id=\x22SVGRepo_tra\
+cerCarrier\x22 stro\
+ke-linecap=\x22roun\
+d\x22 stroke-linejo\
+in=\x22round\x22/>\x0a\x0d<g\
+ id=\x22SVGRepo_ico\
+nCarrier\x22> <path\
+ d=\x22M4 5L15 5\x22 s\
+troke=\x22#ffffff\x22 \
+stroke-width=\x222\x22\
+ stroke-linecap=\
+\x22round\x22 stroke-l\
+inejoin=\x22round\x22/\
+> <path d=\x22M4 8H\
+15\x22 stroke=\x22#fff\
+fff\x22 stroke-widt\
+h=\x222\x22 stroke-lin\
+ecap=\x22round\x22 str\
+oke-linejoin=\x22ro\
+und\x22/> <path d=\x22\
+M4 11H11\x22 stroke\
+=\x22#ffffff\x22 strok\
+e-width=\x222\x22 stro\
+ke-linecap=\x22roun\
+d\x22 stroke-linejo\
+in=\x22round\x22/> <pa\
+th d=\x22M18.4563 1\
+3.5423L13.9268 1\
+8.0719C13.6476 1\
+8.3511 13.292 18\
+.5414 12.9048 18\
+.6188L10.8153 19\
+.0367L11.2332 16\
+.9472C11.3106 16\
+.5601 11.5009 16\
+.2045 11.7801 15\
+.9253L16.3096 11\
+.3957M18.4563 13\
+.5423L19.585 12.\
+4135C19.9755 12.\
+023 19.9755 11.3\
+898 19.585 10.99\
+93L18.8526 10.26\
+69C18.4621 9.876\
+4 17.8289 9.8764\
+ 17.4384 10.2669\
+L16.3096 11.3957\
+M18.4563 13.5423\
+L16.3096 11.3957\
+\x22 stroke=\x22#fffff\
+f\x22 stroke-width=\
+\x222\x22 stroke-linec\
+ap=\x22round\x22 strok\
+e-linejoin=\x22roun\
+d\x22/> </g>\x0a\x0d</svg\
+>\
+\x00\x00\x04M\
+<\
+!DOCTYPE svg PUB\
+LIC \x22-//W3C//DTD\
+ SVG 1.1//EN\x22 \x22h\
+ttp://www.w3.org\
+/Graphics/SVG/1.\
+1/DTD/svg11.dtd\x22\
+>\x0a\x0d<!-- Uploaded\
+ to: SVG Repo, w\
+ww.svgrepo.com, \
+Transformed by: \
+SVG Repo Mixer T\
+ools -->\x0a<svg wi\
+dth=\x22800px\x22 heig\
+ht=\x22800px\x22 viewB\
+ox=\x220 0 24 24\x22 f\
+ill=\x22none\x22 xmlns\
+=\x22http://www.w3.\
+org/2000/svg\x22 st\
+roke=\x22#ffffff\x22>\x0a\
+\x0d<g id=\x22SVGRepo_\
+bgCarrier\x22 strok\
+e-width=\x220\x22/>\x0a\x0d<\
+g id=\x22SVGRepo_tr\
+acerCarrier\x22 str\
+oke-linecap=\x22rou\
+nd\x22 stroke-linej\
+oin=\x22round\x22/>\x0a\x0d<\
+g id=\x22SVGRepo_ic\
+onCarrier\x22> <pat\
+h d=\x22M10 12V17\x22 \
+stroke=\x22#ffffff\x22\
+ stroke-width=\x222\
+\x22 stroke-linecap\
+=\x22round\x22 stroke-\
+linejoin=\x22round\x22\
+/> <path d=\x22M14 \
+12V17\x22 stroke=\x22#\
+ffffff\x22 stroke-w\
+idth=\x222\x22 stroke-\
+linecap=\x22round\x22 \
+stroke-linejoin=\
+\x22round\x22/> <path \
+d=\x22M4 7H20\x22 stro\
+ke=\x22#ffffff\x22 str\
+oke-width=\x222\x22 st\
+roke-linecap=\x22ro\
+und\x22 stroke-line\
+join=\x22round\x22/> <\
+path d=\x22M6 10V18\
+C6 19.6569 7.343\
+15 21 9 21H15C16\
+.6569 21 18 19.6\
+569 18 18V10\x22 st\
+roke=\x22#ffffff\x22 s\
+troke-width=\x222\x22 \
+stroke-linecap=\x22\
+round\x22 stroke-li\
+nejoin=\x22round\x22/>\
+ <path d=\x22M9 5C9\
+ 3.89543 9.89543\
+ 3 11 3H13C14.10\
+46 3 15 3.89543 \
+15 5V7H9V5Z\x22 str\
+oke=\x22#ffffff\x22 st\
+roke-width=\x222\x22 s\
+troke-linecap=\x22r\
+ound\x22 stroke-lin\
+ejoin=\x22round\x22/> \
+</g>\x0a\x0d</svg>\
+"
+
+qt_resource_name = b"\
+\x00\x09\
+\x0cxT\x88\
+\x00n\
+\x00e\x00w\x00P\x00r\x00e\x00f\x00i\x00x\
+\x00\x1a\
+\x0a7\xf6\xe7\
+\x00p\
+\x00l\x00u\x00s\x00-\x00l\x00a\x00r\x00g\x00e\x00-\x00s\x00v\x00g\x00r\x00e\x00p\
+\x00o\x00-\x00c\x00o\x00m\x00.\x00s\x00v\x00g\
+\x00\x1c\
+\x0f\xa3\x00\xa7\
+\x00e\
+\x00d\x00i\x00t\x00-\x00c\x00o\x00n\x00t\x00e\x00n\x00t\x00-\x00s\x00v\x00g\x00r\
+\x00e\x00p\x00o\x00-\x00c\x00o\x00m\x00.\x00s\x00v\x00g\
+\x00\x18\
+\x024\xe8\xe7\
+\x00d\
+\x00e\x00l\x00e\x00t\x00e\x00-\x002\x00-\x00s\x00v\x00g\x00r\x00e\x00p\x00o\x00-\
+\x00c\x00o\x00m\x00.\x00s\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x03\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x90\x00\x00\x00\x00\x00\x01\x00\x00\x07\x00\
+\x00\x00\x01\x8b\x06\x8f\x5c\xee\
+\x00\x00\x00\x18\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x8b\x06\x90\x98D\
+\x00\x00\x00R\x00\x00\x00\x00\x00\x01\x00\x00\x02:\
+\x00\x00\x01\x8b\x06\x90 |\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
---
+++ second_main.py
@@ -0,0 +1,193 @@
+
+import sys
+
+from PySide6 import QtSql, QtWidgets
+from PySide6.QtCore import Qt, QModelIndex, QTimer, QSortFilterProxyModel
+from PySide6.QtGui import QStandardItemModel, QStandardItem, QColor, QPainter, QPalette
+from PySide6.QtWidgets import QApplication, QMainWindow, QStyledItemDelegate, QComboBox, QStyleOptionViewItem, \
+    QItemDelegate
+from ui_main import Ui_MainWindow
+
+import sqlite3
+
+
+class ComboBoxDelegate(QStyledItemDelegate):
+    def __init__(self, choices, parent=None):
+        super().__init__()
+        self.choices = choices
+
+    def createEditor(self, parent, option, index):
+        editor = QComboBox(parent)
+        editor.addItems(self.choices)
+        return editor
+
+    def setEditorData(self, editor, index):
+        value = str(index.model().data(index, Qt.EditRole))
+        editor.setCurrentText(value)
+
+    def setModelData(self, editor, model, index):
+        model.setData(index, editor.currentText(), Qt.EditRole)
+
+    #################################################################
+    def paint(self, painter: QPainter, option: QStyleOptionViewItem, index: QModelIndex):
+        if index.column() == 1 and index.data() == "Выполнено":
+            painter.save()
+
+            # Устанавливаем цвет фона ячейки
+            painter.fillRect(option.rect, QColor(0, 200, 0, 200))
+
+            # Отображаем текст ячейки
+            option.palette.setColor(QPalette.Text, Qt.white)
+            # self.drawDisplay(painter, option, option.rect, index.data())
+
+            painter.restore()
+        elif index.column() == 1 and index.data() == "Исполняется":
+            painter.save()
+
+            # Устанавливаем цвет фона ячейки
+            painter.fillRect(option.rect, QColor(200, 200, 0, 200))
+
+            # Отображаем текст ячейки
+            option.palette.setColor(QPalette.Text, Qt.white)
+            # self.drawDisplay(painter, option, option.rect, index.data())
+
+            painter.restore()
+        else:
+            # Если условие не выполняется, отображаем ячейку по умолчанию
+            super().paint(painter, option, index)
+
+
+class ClientsList(QMainWindow):
+    def __init__(self):
+        super(ClientsList, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        ######################
+        self.create_connection()
+        ###################
+
+        self.connection = sqlite3.connect('clients_db.db')
+        self.cursor = self.connection.cursor()
+
+        # self.cursor.execute('SELECT * FROM clients ORDER BY Готовность, id')
+        # self.data = self.cursor.fetchall()
+
+        ################
+        self.model = QStandardItemModel()
+        self.ui.tableView.setModel(self.model)
+        #####################
+        self.view_data()
+
+        self.ui.pushButton.clicked.connect(self.add_new_client)
+        self.ui.pushButton_2.clicked.connect(self.delete_client)
+
+        # self.ui.tableView.hideColumn(0)
+        # self.model.dataChanged.connect(self.update_database)
+        self.model.dataChanged.connect(self.update_table_and_database)
+        self.model.rowsInserted.connect(self.add_new_client_db)
+
+        delegate = ComboBoxDelegate(["Нет лр", "Исполняется", "Выполнено"], self.ui.tableView)
+        self.ui.tableView.setItemDelegateForColumn(1, delegate)
+
+    #############3
+        
+    ##########33##
+
+    def create_connection(self):
+        db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+        db.setDatabaseName('clients_db.db')
+
+        if not db.open():
+            QtWidgets.QMessageBox.critical(None, "Не получилось открыть базу данных:(",
+                                           "Нажмите, чтобы закрыть", QtWidgets.QMessageBox.Cancel)
+            return False
+
+        query = QtSql.QSqlQuery()
+        query.exec("CREATE TABLE IF NOT EXISTS clients (id integer primary key AUTOINCREMENT, Готовность VARCHAR(40), "
+                   "Имя VARCHAR(60), Приведенные VARCHAR(100), Комментарий VARCHAR(160))")
+        return True
+    def view_data(self):
+        # self.model.clear()
+        self.model.setHorizontalHeaderLabels(["id", "Готовность", "Имя", "Приведенные", "Комментарий"])
+        self.cursor.execute('SELECT * FROM clients ORDER BY Готовность')
+        self.data = self.cursor.fetchall()
+
+        for i_row, row_item in enumerate(self.data):
+            for i_col, col_item in enumerate(row_item):
+                if i_col == 0:
+                    cell = QStandardItem()
+                    cell.setData(col_item, Qt.DisplayRole)
+                    self.model.setItem(i_row, i_col, cell)
+                else:
+                    cell = QStandardItem(str(col_item))
+                    self.model.setItem(i_row, i_col, cell)
+        #######################3
+
+        ##########################
+
+        print("view_data")
+
+    def update_table_and_database(self, top_left_index, bottom_right_index):
+        self.update_database(bottom_right_index)  # Обновление базы данных
+        self.sort_table()
+
+    def sort_table(self):
+        # self.model.sort(5, Qt.AscendingOrder)  # Сортировка по столбцу "id_1"
+        print("sort1")
+        self.model.sort(1, Qt.AscendingOrder)  # Сортировка по столбцу "Готовность"
+        print("sort2")
+
+    def add_new_client(self):
+        rowPosition = self.model.rowCount()
+        self.model.insertRow(rowPosition)
+
+    def update_database(self, index):
+        row = index.row()
+        col = index.column()
+        new_value = index.data()
+        id_index = self.model.index(row, 0)
+        id_value = self.model.data(id_index)
+        column_name = self.model.horizontalHeaderItem(col).text()
+
+        self.cursor.execute(f'UPDATE clients SET {column_name} = ? WHERE id = ?', (new_value, id_value))
+        self.connection.commit()
+        # self.view_data()
+        print("update")
+
+    def add_new_client_db(self):
+        self.cursor.execute("INSERT INTO clients (Готовность, Имя, Приведенные, Комментарий) VALUES ('', '', '', '')")
+        self.connection.commit()
+
+        self.cursor.execute('SELECT COUNT(*) FROM clients')
+        row_count = self.cursor.fetchone()[0]
+        row_index = row_count - 1
+
+        self.cursor.execute('SELECT * FROM clients ORDER BY Готовность')
+        self.data = self.cursor.fetchall()
+        item = max([self.data[i][0] for i in range(len(self.data))]) if len(self.data) != 1 else 1
+        cell = QStandardItem(str(item))
+        self.model.setItem(row_index, 0, cell)
+        # self.view_data()
+        print("add")
+
+    def delete_client(self):
+        selected_index = self.ui.tableView.currentIndex()
+        if not selected_index.isValid():
+            return None
+
+        row = selected_index.row()
+        id_value = self.data[row][0]
+        self.model.removeRow(row)
+
+        self.cursor.execute("DELETE FROM clients WHERE ID=?", (id_value,))
+        self.connection.commit()
+        self.cursor.execute('SELECT * FROM clients ORDER BY Готовность')
+        self.data = self.cursor.fetchall()
+        print("del")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = ClientsList()
+    window.show()
+    sys.exit(app.exec())
